bbbb77e9cb3543e8f6f799714b87c7185cd7fda3
xuechong <xuechong87@gmail.com>
2013-01-29 11:02:42 +0800

    the ctrl+1 & ctrl+2 shortcut are avaliable now

---
+++ xingxing/background.js
@@ -0,0 +1,23 @@
+﻿chrome.commands.onCommand.addListener(function(command) {
+	if (command == "tab-back") {
+		chrome.tabs.getSelected(null, function(tab) {
+			chrome.tabs.query({"index":tab.index-1}, function (results){
+				updateTab(results);
+			});
+		});
+	}
+	
+	if (command == "tab-forward") {
+		chrome.tabs.getSelected(null, function(tab) {
+			chrome.tabs.query({"index":tab.index+1}, function (results){
+				updateTab(results);
+			});
+		});
+	}
+});
+
+function updateTab(queryResults){
+	if(queryResults[0]){
+		chrome.tabs.update(queryResults[0].id, {'selected': true});
+	}
+}
